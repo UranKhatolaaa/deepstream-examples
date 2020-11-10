a7b7992de4bb4f599ad1ee21ececd423e4735144
Frank Sepulveda <socieboy@gmail.com>
2020-11-10 15:21:22 -0800

    UpdatE

--- rtmp-as-source.py
+++ rtmp-as-source.py
@@ -1,6 +1,8 @@
 #
 # Publish video to Ant Server
 #
+# gst-launch-1.0 uridecodebin uri=https://media.streamit.link:5443/LiveApp/streams/44c2fc1d83d2ccf2dab09d311aa6da4e.m3u8 ! autovideosink
+
 import argparse
 import sys
 sys.path.append('../')
@@ -25,34 +27,34 @@ def main():
         return False
     
     # Create GST Elements
-    source = create_element_or_error("rtmpsrc", "rtmp-source")
+    source = create_element_or_error("uridecodebin", "uri-source")
     
     # encoder = create_element_or_error("nvv4l2h264enc", "encoder")
     # parser = create_element_or_error("h264parse", "parser")
-    # muxer = create_element_or_error("flvmux", "muxer")
-    sink = create_element_or_error("rtmpsink", "sink")
+    sink = create_element_or_error("autovideosink", "sink")
 
-    if not (source or encoder or parseer or muxer or sink):
-        return
+    # if not (source or encoder or parseer or muxer or sink):
+    #     return
 
-    # Set Element Properties
-    source.set_property('sensor-id', 0)
-    sink.set_property('location', 'rtmp://media.streamit.link/LiveApp/streaming-test')
+    # # Set Element Properties
+    source.set_property('uri', 'https://media.streamit.link:5443/LiveApp/streams/44c2fc1d83d2ccf2dab09d311aa6da4e.m3u8')
+    # source.set_property('do-timestamp', True)
+    # sink.set_property('location', 'rtmp://media.streamit.link/LiveApp/streaming-test')
 
     # Add Elemements to Pipielin
     print("Adding elements to Pipeline")
     pipeline.add(source)
-    pipeline.add(encoder)
-    pipeline.add(parser)
-    pipeline.add(muxer)
+    # pipeline.add(encoder)
+    # pipeline.add(parser)
+    # pipeline.add(muxer)
     pipeline.add(sink)
 
     # Link the elements together:
     print("Linking elements in the Pipeline")
-    source.link(encoder)
-    encoder.link(parser)
-    parser.link(muxer)
-    muxer.link(sink)
+    source.link(sink)
+    # encoder.link(parser)
+    # parser.link(muxer)
+    # muxer.link(sink)
     
     # Create an event loop and feed gstreamer bus mesages to it
     loop = GObject.MainLoop()
