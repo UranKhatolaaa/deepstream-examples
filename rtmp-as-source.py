c87cbcc130b8912c1d8c54fb4fe6d03cb8178bc3
Frank Sepulveda <socieboy@gmail.com>
2020-11-10 14:13:21 -0800

    New example

---
+++ rtmp-as-source.py
@@ -0,0 +1,76 @@
+#
+# Publish video to Ant Server
+#
+import argparse
+import sys
+sys.path.append('../')
+
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst
+from common.is_aarch_64 import is_aarch64
+from common.bus_call import bus_call
+from common.create_element_or_error import create_element_or_error
+
+def main():
+    
+    # Standard GStreamer initialization
+    GObject.threads_init()
+    Gst.init(None)
+
+    # Create Pipeline Element
+    pipeline = Gst.Pipeline()
+    if not pipeline:
+        print("Unable to create Pipeline")
+        return False
+    
+    # Create GST Elements
+    source = create_element_or_error("rtmpsrc", "rtmp-source")
+    
+    # encoder = create_element_or_error("nvv4l2h264enc", "encoder")
+    # parser = create_element_or_error("h264parse", "parser")
+    # muxer = create_element_or_error("flvmux", "muxer")
+    sink = create_element_or_error("rtmpsink", "sink")
+
+    if not (source or encoder or parseer or muxer or sink):
+        return
+
+    # Set Element Properties
+    source.set_property('sensor-id', 0)
+    sink.set_property('location', 'rtmp://media.streamit.link/LiveApp/streaming-test')
+
+    # Add Elemements to Pipielin
+    print("Adding elements to Pipeline")
+    pipeline.add(source)
+    pipeline.add(encoder)
+    pipeline.add(parser)
+    pipeline.add(muxer)
+    pipeline.add(sink)
+
+    # Link the elements together:
+    print("Linking elements in the Pipeline")
+    source.link(encoder)
+    encoder.link(parser)
+    parser.link(muxer)
+    muxer.link(sink)
+    
+    # Create an event loop and feed gstreamer bus mesages to it
+    loop = GObject.MainLoop()
+    bus = pipeline.get_bus()
+    bus.add_signal_watch()
+    bus.connect ("message", bus_call, loop)
+
+    # Start play back and listen to events
+    print("Starting pipeline")
+    pipeline.set_state(Gst.State.PLAYING)
+
+    try:
+        loop.run()
+    except:
+        pass
+
+    # Cleanup
+    pipeline.set_state(Gst.State.NULL)
+
+if __name__ == "__main__":
+    sys.exit(main())
