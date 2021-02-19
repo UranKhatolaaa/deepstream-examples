86403a8dd95846024c26e9e33cea3ff84c786566
Frank Sepulveda <socieboy@gmail.com>
2021-02-19 15:37:27 -0800

    tracker

--- display-tracker.py
+++ display-tracker.py
@@ -13,6 +13,7 @@ from gi.repository import GObject, Gst
 from common.is_aarch_64 import is_aarch64
 from common.bus_call import bus_call
 from common.create_element_or_error import create_element_or_error
+from common.object_detection import osd_sink_pad_buffer_probe
 
 def main():
     
@@ -20,17 +21,22 @@ def main():
     GObject.threads_init()
     Gst.init(None)
 
-
     # Create Pipeline Element
     pipeline = Gst.Pipeline()
     if not pipeline:
         sys.stderr.write(" Unable to create Pipeline")
         return
     
-    source = create_element_or_error("nvarguscamerasrc", "camera-source")
+    # source = create_element_or_error("nvarguscamerasrc", "camera-source")
+    # src_caps = create_element_or_error("capsfilter", "source-caps-definition")
+    # src_caps.set_property("caps", Gst.Caps.from_string("video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, framerate=30/1, format=(string)NV12"))
+
     streammux = create_element_or_error("nvstreammux", "Stream-muxer")
-    pgie = create_element_or_error("nvinfer", "primary-inference")
-    convertor = create_element_or_error("nvvidconv", "converter-1")
+    pgie = Gst.ElementFactory.make("nvinfer", "primary-inference")
+    tracker = create_element_or_error("nvtracker", "tracker")
+    convertor = Gst.ElementFactory.make("nvvideoconvert", "convertor-1")
+    nvosd = Gst.ElementFactory.make("nvdsosd", "onscreendisplay")
+    convertor2 = Gst.ElementFactory.make("nvvidconv", "converter-2")
     transform = create_element_or_error("nvegltransform", "nvegl-transform")
     sink = create_element_or_error("nveglglessink", "egl-overlay")
 
@@ -45,47 +51,58 @@ def main():
     streammux.set_property('batch-size', 1)
     streammux.set_property('batched-push-timeout', 4000000)
 
-    pgie.set_property('config-file-path', "./nv-inferance-config-files/config_infer_primary_peoplenet.txt")
-    # convertor.set_property('flip-method', 2)
+    pgie.set_property('config-file-path', "./nv-inferance-config-files/config_infer_primary_trafficcamnet.txt")
 
+    #Set properties of tracker
+    tracker.set_property('tracker-width', 640)
+    tracker.set_property('tracker-height', 384)
+    tracker.set_property('ll-lib-file', '/opt/nvidia/deepstream/deepstream-5.0/lib/libnvds_nvdcf.so')
+    tracker.set_property('gpu-id', 0)
+    tracker.set_property('enable-batch-process', 1)
+    tracker.set_property('enable-past-frame', 1)
+    tracker.set_property('ll-config-file', './tracker_config.yml')
 
     # Add Elemements to Pipielin
-    print("Adding elements to Pipeline")
     pipeline.add(source)
+    # pipeline.add(src_caps)
     pipeline.add(streammux)
     pipeline.add(pgie)
+    pipeline.add(tracker)
     pipeline.add(convertor)
+    pipeline.add(nvosd)
+    pipeline.add(convertor2)
+    pipeline.add(transform)
     pipeline.add(sink)
-    if is_aarch64():
-        pipeline.add(transform)
-
 
     sinkpad = streammux.get_request_pad("sink_0")
     if not sinkpad:
         sys.stderr.write(" Unable to get the sink pad of streammux")
 
-
     # Link the elements together:
-    print("Linking elements in the Pipeline")
     source.link(streammux)
+    # src_caps.link(streammux)
     streammux.link(pgie)
-    pgie.link(convertor)
-    if is_aarch64():
-        convertor.link(transform)
-        transform.link(sink)
-    else:
-        convertor.link(sink)
+    pgie.link(tracker)
+    tracker.link(convertor)
+    convertor.link(nvosd)
+    nvosd.link(convertor2)
+    convertor2.link(transform)
+    transform.link(sink)
     
-
     # Create an event loop and feed gstreamer bus mesages to it
     loop = GObject.MainLoop()
     bus = pipeline.get_bus()
     bus.add_signal_watch()
     bus.connect ("message", bus_call, loop)
 
+    #Feed tracker
+    tracker_sinkpad = tracker.get_static_pad("sink")
+    if not tracker_sinkpad:
+        sys.stderr.write(" Unable to get sink pad of nvosd")
+
+    tracker_sinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
 
     # Start play back and listen to events
-    print("Starting pipeline")
     pipeline.set_state(Gst.State.PLAYING)
 
     try:
--- stream-to-rtmp-tracker.py
+++ stream-to-rtmp-tracker.py
@@ -18,80 +18,70 @@ PGIE_CLASS_ID_BICYCLE = 1
 PGIE_CLASS_ID_PERSON = 2
 PGIE_CLASS_ID_ROADSIGN = 3
 
+object_counter = {
+    PGIE_CLASS_ID_VEHICLE : 0,
+    PGIE_CLASS_ID_PERSON : 0,
+    PGIE_CLASS_ID_BICYCLE : 0,
+    PGIE_CLASS_ID_ROADSIGN : 0
+}
 
-def osd_sink_pad_buffer_probe(pad,info,u_data):
-    frame_number=0
-
-    obj_counter = {
-        PGIE_CLASS_ID_VEHICLE:0,
-        PGIE_CLASS_ID_PERSON:0,
-        PGIE_CLASS_ID_BICYCLE:0,
-        PGIE_CLASS_ID_ROADSIGN:0
-    }
-
-    num_rects=0
-
+def tracker_sink_pad_buffer_probe(pad, info, u_data):
+    
     gst_buffer = info.get_buffer()
     if not gst_buffer:
-        print("Unable to get GstBuffer ")
+        print("Unable to get GstBuffer")
         return
 
-
     batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
+    frame_list = batch_meta.frame_meta_list
 
-    l_frame = batch_meta.frame_meta_list
-
-    while l_frame is not None:
-
+    while frame_list is not None:
         try:
-            frame_meta = pyds.NvDsFrameMeta.cast(l_frame.data)
+            frame_meta = pyds.NvDsFrameMeta.cast(frame_list.data)
         except StopIteration:
             break
 
-        frame_number=frame_meta.frame_num
-        num_rects = frame_meta.num_obj_meta
-        l_obj=frame_meta.obj_meta_list
-        
-        while l_obj is not None:
+        list_of_objects = frame_meta.obj_meta_list
+
+        while list_of_objects is not None:
+            
             try:
-                obj_meta=pyds.NvDsObjectMeta.cast(l_obj.data)
-                # print(obj_meta.rect_params.top)
-                # print(obj_meta.rect_params.left)
-                # print(obj_meta.rect_params.width)
-                # print(obj_meta.rect_params.height)
-                # print(obj_meta.class_id)
-                # print(obj_meta.obj_label)
-                # print(obj_meta.object_id)
+                object_meta = pyds.NvDsObjectMeta.cast(list_of_objects.data)
+                # https://docs.nvidia.com/metropolis/deepstream/5.0DP/python-api/NvDsMeta/NvDsObjectMeta.html
+                # print(object_meta.rect_params.top)
+                # print(object_meta.rect_params.left)
+                # print(object_meta.rect_params.width)
+                # print(object_meta.rect_params.height)
+                # print(object_meta.class_id)
+                # print(object_meta.obj_label)
+                # print(object_meta.object_id)
+
             except StopIteration:
                 break
-            obj_counter[obj_meta.class_id] += 1
+
+            # object_counter[object_meta.class_id] += 1
+
             try: 
-                l_obj=l_obj.next
+                list_of_objects = list_of_objects.next
             except StopIteration:
                 break
 
-        display_meta=pyds.nvds_acquire_display_meta_from_pool(batch_meta)
-        display_meta.num_labels = 1
-        py_nvosd_text_params = display_meta.text_params[0]
-        py_nvosd_text_params.display_text = "Frames: {} | Objects: {} | Vehicles: {} | Persons: {}".format(frame_number, num_rects, obj_counter[PGIE_CLASS_ID_VEHICLE], obj_counter[PGIE_CLASS_ID_PERSON])
-
-        py_nvosd_text_params.x_offset = 10
-        py_nvosd_text_params.y_offset = 12
-
-        py_nvosd_text_params.font_params.font_name = "Serif"
-        py_nvosd_text_params.font_params.font_size = 12
-        py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
-
-        py_nvosd_text_params.set_bg_clr = 1
-        py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
-        pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+            display_meta=pyds.nvds_acquire_display_meta_from_pool(batch_meta)
+            display_meta.num_rects = 1
+            py_nvosd_rect_params = display_meta.rect_params[0]
+            print('hello')
+            # py_nvosd_rect_params.top = object_meta.rect_params.top
+            # py_nvosd_rect_params.left = object_meta.rect_params.left
+            # py_nvosd_rect_params.width = object_meta.rect_params.width
+            # py_nvosd_rect_params.height = object_meta.rect_params.height
+            # pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+            print('llego')
         try:
-            l_frame=l_frame.next
+            frame_list = frame_list.next
         except StopIteration:
             break
-			
-    return Gst.PadProbeReturn.OK
 
+    return Gst.PadProbeReturn.OK
 
 def main():
     
@@ -113,7 +103,7 @@ def main():
     streammux = create_element_or_error("nvstreammux", "Stream-muxer")
     pgie = create_element_or_error("nvinfer", "primary-inference")
     convertor = create_element_or_error("nvvideoconvert", "convertor-1")
-    nvosd = create_element_or_error("nvdsosd", "onscreendisplay")
+    tracker = create_element_or_error("nvtracker", "tracker")
     convertor2 = create_element_or_error("nvvideoconvert", "convertor-2")
     caps = create_element_or_error("capsfilter", "filter-convertor-2")
     
@@ -134,10 +124,15 @@ def main():
     streammux.set_property('width', 1280)
     streammux.set_property('height', 720)
     streammux.set_property('num-surfaces-per-frame', 1)
-    streammux.set_property('batch-size', 1)
-    streammux.set_property('batched-push-timeout', 4000000)
+    streammux.set_property('batch-size', 30)
+    streammux.set_property('batched-push-timeout', 25000)
 
-    pgie.set_property('config-file-path', "./nv-inferance-config-files/config_infer_primary_peoplenet.txt")
+    tracker.set_property('ll-lib-file', '/opt/nvidia/deepstream/deepstream-5.0/lib/libnvds_nvdcf.so')
+    tracker.set_property('gpu-id', 0)
+    tracker.set_property('enable-batch-process', 1)
+    tracker.set_property('ll-config-file', '/opt/nvidia/deepstream/deepstream-5.0/samples/configs/deepstream-app/tracker_config.yml')
+
+    pgie.set_property('config-file-path', "./nv-inferance-config-files/config_infer_primary_facedetectir.txt")
     sink.set_property('location', 'rtmp://media.streamit.live/LiveApp/streaming-test')
 
     # Add Elemements to Pipielin
@@ -147,7 +142,7 @@ def main():
     pipeline.add(streammux)
     pipeline.add(pgie)
     pipeline.add(convertor)
-    pipeline.add(nvosd)
+    pipeline.add(tracker)
     pipeline.add(convertor2)
     pipeline.add(encoder)
     pipeline.add(parser)
@@ -164,8 +159,8 @@ def main():
     src_caps.link(streammux)
     streammux.link(pgie)
     pgie.link(convertor)
-    convertor.link(nvosd)
-    nvosd.link(convertor2)
+    convertor.link(tracker)
+    tracker.link(convertor2)
     convertor2.link(encoder)
     encoder.link(parser)
     parser.link(muxer)
@@ -178,11 +173,11 @@ def main():
     bus.connect ("message", bus_call, loop)
 
     print('Create OSD Sink Pad')
-    osdsinkpad = nvosd.get_static_pad("sink")
-    if not osdsinkpad:
+    tracker_sinkpad = tracker.get_static_pad("sink")
+    if not tracker_sinkpad:
         sys.stderr.write(" Unable to get sink pad of nvosd")
 
-    osdsinkpad.add_probe(Gst.PadProbeType.BUFFER, osd_sink_pad_buffer_probe, 0)
+    tracker_sinkpad.add_probe(Gst.PadProbeType.BUFFER, tracker_sink_pad_buffer_probe, 0)
 
     # Start play back and listen to events
     print("Starting pipeline")
